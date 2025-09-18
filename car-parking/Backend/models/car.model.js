ee2cb017202c9dd30eb7cae72dd8b09443e4624d
Few-Kittanart <deadhunter587@gmail.com>
2025-09-18 14:07:35 +0700

    .

--- car-parking/Backend/models/car.model.js
+++ car-parking/Backend/models/car.model.js
@@ -6,6 +6,7 @@ const carSchema = new mongoose.Schema({
   car_registration: { type: String, required: true },
   car_registration_province: { type: String, required: true },
   brand_car: { type: String },
+  model_car: { type: String },
   type_car: { type: String },
   color: { type: String },
   service_history: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ServiceHistory' }],
